a5c4592c648674a94ac38c9488b8eddee3960853
Tatu Romero <tatiana.romero@ucuenca.edu.ec>
2024-09-27 23:13:47 -0500

    Se realizaron algunos cambios al código del cursor.

--- sketch.js
+++ sketch.js
@@ -1,21 +1,21 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(4);
+  frameRate(80);
   background(0);
   rectMode(CENTER);
 }
 
 function draw() {
-  background(random(150, 250), random(150, 200), 150, 20);
+  background(random(170, 190), random(255, 200), 47, 20);
 
-  strokeWeight(5);
-  noFill();
-  stroke(mouseY, 255, 0);
-  fill(mouseY, 0, 30);
-  circle(mouseX + 80, mouseY, mouseX);
-
+  circle(mouseX - 80, mouseY - 50, mouseX);
   strokeWeight(1);
-  rect(mouseX, mouseY, 110, 110);
-  fill(150, 0, 150);
+  // noFill();
+  stroke(mouseY, 255, 0);
+  // fill(mouseY, 0, 30);
+
+  rect(mouseX, mouseY, 500, 110);
+  strokeWeight(1);
+  // fill(150, 0, 150);
   stroke(mouseX, mouseY, 100, 100);
 }
